e495f54e39b4356c9d3c9ea72668a5b81632819d
DangoMelon <gerardo_art@me.com>
2021-06-16 22:25:41 -0500

    Remove nz from coordinates

--- ifa_reader.py
+++ ifa_reader.py
@@ -30,26 +30,26 @@ dfs = [df[splits[i] : splits[i + 1]] for i in range(len(splits) - 1)]
 def to_dataarray(dataframe):
     year, month, day, hour, *_ = dataframe.iloc[0].T.values
     dataframe = dataframe.replace(-999, np.nan)
-    raw_data = dataframe.iloc[1:]
+    raw_data = dataframe.iloc[2:]
     xdata = xr.Dataset(
         {
-            "z": (["nz"], raw_data[1], dict(long_name="Height")),
-            "t": (["nz"], raw_data[2], dict(long_name="Temperature")),
+            "z": (["pr"], raw_data[1], dict(long_name="Height")),
+            "t": (["pr"], raw_data[2], dict(long_name="Temperature")),
             "q": (
-                ["nz"],
+                ["pr"],
                 raw_data[3],
                 dict(long_name="Specific Humidity", units="g/kg"),
             ),
-            "rh": (["nz"], raw_data[4], dict(long_name="Relative Humidity", units="%")),
-            "u": (["nz"], raw_data[5], dict(long_name="Zonal-wind component")),
-            "v": (["nz"], raw_data[6], dict(long_name="Meridional-wind component")),
+            "rh": (["pr"], raw_data[4], dict(long_name="Relative Humidity", units="%")),
+            "u": (["pr"], raw_data[5], dict(long_name="Zonal-wind component")),
+            "v": (["pr"], raw_data[6], dict(long_name="Meridional-wind component")),
         },
         coords={
             "time": pd.to_datetime(
                 f"{year+1900:.0f}-{month:02.0f}-{day:02.0f} {hour}:00"
             ),
-            "nz": np.arange(raw_data[0].size),
-            "pr": (["nz"], raw_data[0], dict(long_name="Pressure", units="hPa")),
+            # "nz": np.arange(raw_data[0].size),
+            "pr": (["pr"], raw_data[0], dict(long_name="Pressure", units="hPa")),
         },
     )
     return xdata
