0073d870603b9b3001f4d7e30849e956557cd190
DangoMelon <gerardo_art@me.com>
2021-06-16 22:55:34 -0500

    Process deriv data

--- ifa_reader.py
+++ ifa_reader.py
@@ -33,8 +33,8 @@ def stacked_data(file):
 # %%
 
 
-def to_dataarray(dataframe):
-    year, month, day, hour, *_ = dataframe.iloc[0].T.values
+def basic_to_dataset(dataframe):
+    year, month, day, hour, *_ = dataframe.iloc[0].values
     dataframe = dataframe.replace(-999, np.nan)
     raw_data = dataframe.iloc[2:]
     xdata = xr.Dataset(
@@ -61,14 +61,50 @@ def to_dataarray(dataframe):
     return xdata
 
 
+def deriv_to_dataset(dataframe):
+    year, month, day, hour, *_ = dataframe.iloc[0].values
+    dataframe = dataframe.replace(-999, np.nan)
+    raw_data = dataframe.iloc[2:]
+    xdata = xr.Dataset(
+        {
+            "div": (
+                ["pr"],
+                raw_data[1],
+                dict(long_name="Horizontal Divergence", units="1/s *10^6"),
+            ),
+            "w": (["pr"], raw_data[2], dict(long_name="Omega", units="mb/hr")),
+            "q1": (
+                ["pr"],
+                raw_data[3],
+                dict(long_name="Apparent Heating", units="K/day"),
+            ),
+            "q2": (
+                ["pr"],
+                raw_data[4],
+                dict(long_name="Apparent Drying", units="K/day"),
+            ),
+        },
+        coords={
+            "time": pd.to_datetime(
+                f"{year+1900:.0f}-{month:02.0f}-{day:02.0f} {hour}:00"
+            ),
+            # "nz": np.arange(raw_data[0].size),
+            "pr": (["pr"], raw_data[0], dict(long_name="Pressure", units="hPa")),
+        },
+    )
+    return xdata
+
+
 # %%
 basic_dfs = stacked_data(file_basic)
 deriv_dfs = stacked_data(file_deriv)
 
-ds_container = [to_dataarray(x) for x in dfs]
-xdata = xr.concat(ds_container, dim="time")
+xdata_basic = xr.concat([basic_to_dataset(x) for x in basic_dfs], dim="time")
+xdata_deriv = xr.concat([deriv_to_dataset(x) for x in basic_dfs], dim="time")
 # %%
 
 # Save the data
-xdata.to_netcdf("basic_flds.nc")
+xdata_basic.to_netcdf("basic_flds.nc")
+xdata_deriv.to_netcdf("deriv_flds.nc")
+
 # %%
